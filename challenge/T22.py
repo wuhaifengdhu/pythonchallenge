4001044108db3c598eb7514de9a427e603be10ba
haifwu <haifwu@paypal.com>
2017-01-09 09:44:37 +0800

    added level 22

--- challenge/T22.py
+++ challenge/T22.py
@@ -8,7 +8,7 @@ from lib.text_helper import TextHelper
 from lib.web_helper import WebHelper
 from lib.image_helper import ImageHelper
 from cStringIO import StringIO
-from PIL import Image
+from PIL import Image, ImageDraw
 
 
 class T22(Challenge):
@@ -19,9 +19,36 @@ class T22(Challenge):
         print "Get gif url: %s" % gif_url
 
         ImageHelper.show_image_from_web(gif_url, self.user, self.password)
-        # local_image = WebHelper.get_url_page(gif_url)
-        # WebHelper.download_with_auth(gif_url, local_image, self.user, self.password)
-        # ImageHelper.show_in_windows(local_image)
+        local_image = WebHelper.get_url_page(gif_url)
+        WebHelper.download_with_auth(gif_url, local_image, self.user, self.password)
+        ImageHelper.show_from_file(local_image)
+
+        # step 2, get Image draw
+        img = Image.open(local_image)
+        prompt_img = ImageDraw.Draw("RGB", (500, 200))
+        draw = ImageDraw.Draw(prompt_img)
+        cx, cy = 0, 100
+        for frame in range(img.n_frames):
+            img.seek(frame)
+            left, upper, right, lower = img.getbbox()
+            dx = left - 100
+            dy = upper - 100
+
+            if dx == dy == 0:
+                cx += 50
+                cy = 100
+
+            cx += dx
+            cy += dy
+
+            draw.point([cx, cy])
+        prompt_img.show()  # shows 'bonus'
+
+        # step 3, set prompt
+        prompt_words = 'bonus'
+        print "get prompt words from image %s" % prompt_words
+        self.set_prompt(prompt_words)
+
 
 if __name__ == '__main__':
     current_url = 'http://www.pythonchallenge.com/pc/hex/copper.html'
